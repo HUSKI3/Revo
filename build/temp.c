1bb136e540f1fc8ebcbec9cf70e59611fac02ee7
HUSKI3 <huskielunar@gmail.com>
2020-08-03 17:36:34 +0200

    Automatic deploy

--- build/temp.c
+++ build/temp.c
@@ -1,15 +1,24 @@
 #include <stdio.h> 
- int main() { //Revo  version  1.0.4  numberswapper.rv// ;
- double  first,  second,  temp   ;
- printf  ("Enter  first  number:  ")   ;
- scanf  ("%lf",  &first)   ;
- printf  ("Enter  second  number:  ")   ;
- scanf  ("%lf",  &second)   ;
- temp  =  first   ;
- first  =  second   ;
- second  =  temp   ;
- printf  ("\nAfter  swapping,  firstNumber  =  %.2lf\n",  first)   ;
- printf  ("After  swapping,  secondNumber  =  %.2lf\n",  second)   ;
+ int main() { double  number,  second,  sum  =  0 ;
+ printf  ("Nat  is  cute  \n") ;
+ printf  ("I\n") ;
+ printf  ("              .....                      .....\n") ;
+ printf  ("      ,ad8PPPP88b,          ,d88PPPP8ba,\n") ;
+ printf  ("    d8P                Y8b,  ,d8P                Y8b\n") ;
+ printf  ("  dP                          8a8                        `Yd\n") ;
+ printf  ("  8(                            v                            )8\n") ;
+ printf  ("  I8                                                          8I\n") ;
+ printf  ("    Yb,                                                  ,dP\n") ;
+ printf  ("        8a,                                          ,a8  \n") ;
+ printf  ("            8a,                                  ,a8  \n") ;
+ printf  ("                Yba                          adP  \n") ;
+ printf  ("                    Y8a                  a8P  \n") ;
+ printf  ("                        Y8,          ,88  \n") ;
+ printf  ("                            8b      d8\n") ;
+ printf  ("                              8b  d8\n") ;
+ printf  ("                                888\n") ;
+ printf  ("                                  v  ") ;
+ printf  ("You\n\n") ;
 
 return 0;
 }
